9c223521d1011d7ed3c09394b32c213d0bbbe0c9
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-10-23 03:42:08 -0400

    initial html pages, mild CSS, and empty JS

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "Inside which tag of an HTML document do you put the JavaScript?",
+        choices: ["<java>", "<body>", "<script>", "<head>"],
+        answer: "<script>"
+    },
+    {
+        title: "The condition in an if/else statement is enclosed within ______.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What is the syntax for referencing an external script called 'abc.js'?",
+        choices: ["<script class='abc.js'>", "<script href='abc.js'>", "<script link='abc.js'>", "<script src='abc.js'>"],
+        answer: "<script src='abc.js'>"
+    },
+    {
+        title: "Which type of pop up box will allow a user to type a response?",
+        choices: ["input", "prompt", "alert", "confirm"],
+        answer: "prompt"
+    },
+    {
+        title: "What is a DOM in JavaScript?",
+        choices: ["Data of Mine", "Document of Master", "Data Object Modal", "Document Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "Is JS case-sensitive?",
+        choices: ["Yes", "No"],
+        answer: "Yes"
+    },
+];
