58fe41079f810e7d6d88caba59ad6580b28245ca
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-10-24 04:04:16 -0400

    the timer and hiding elements  work well, but can't get questions to populate. STUCK and Confused. README also written

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Code-Quiz
+# [Code-Quiz] (https://lbernadel.github.io/Code-Quiz/)
+
+![Code Quiz Screenshot](code-quiz.JPG)
+
+## Description
+The purpose of this project was to create a timer-based JavaScript coding assessment. The application has a few multiple choice questions and stores high scores on the user's device. The number of questions decides the length of time for the quiz (15 seconds/question) once the user clicks "Start" and they are penalized if they answer a question incorrectly. The final score is based on the amount remaining on the timer.
+
+Relevant new subject matter from the week: DOM traversal, Local Storage,
+
+## Challenges
+This entire activity was very difficult. I really don't understand what I'm doing with the DOM and Local storage and it took forever just to figure out how to get my timer working. I don't understand how to display the questions individually or if I did the JSON parse correctly, so I couldn't move forward afterwards. I tried looking at online sources and class examples multiple times and was still confused after 2 days with several hours of the same cycle.
+
+### Sources
+- textContent vs innerHTML/innerText 
+  - https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#Differences_from_innerText
+
+- Hidden attributes
+  - https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/hidden
+  - https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/hidden
+
+- Using JSON 
+  - https://code-boxx.com/json-php-javascript-beginners/
+  - https://www.w3schools.com/js/js_json_stringify.asp
--- questions.js
+++ questions.js
@@ -31,33 +31,61 @@ var questions = [
     },
 ];
 
-var startBtn = document.getElementsByClassName("startBtn"),
+var startBtn = document.getElementById("startBtn"),
     redoBtn = document.getElementsByClassName("redoBtn"),
     clearBtn = document.getElementsByClassName("clearBtn"),
     timerEl = document.getElementById("timer"),
-    submitScoreEl = document.getElementsByClassName("submit-score"),
+    submitScoreEl = document.querySelector("#submit-score"),
     userScore = document.getElementById("user-score"),
     newScore = document.getElementsByClassName("new-score"),
     submitBtn = document.querySelector("button.submitBtn"),
-    secondsLeft = 90;
+    quizDiv = document.getElementById("questions"),
+    secondsLeft = (questions.length * 15 + 1);
+
+function startTimer () {
+    event.stopPropagation;
+// swap welcome msg w/ questions
+    document.getElementById("home").setAttribute("hidden", true);
+    document.getElementById("questions").setAttribute("hidden", false);
+
+// timer set and begins 90s countdown
+    setTimer();
+
+    while (setTimer) {
+        // display questions individually
+        makeQuestions(questions); 
+        
+    };
+
+}
 
 function setTimer(){
-    var timerInterval = setInterval(function() {
+    
+    var countdown = setInterval(function() {
         secondsLeft--;
-        timerEl.textContent = "Timer: " + secondsLeft;
+        timerEl.textContent = "Time: " + secondsLeft;
 
         if (secondsLeft === 0) {
+            clearInterval(countdown);
             // display option to enter name to scoreboard
-            // 
+            // display score
         }
 
 
     }, 1000);
 }
 
-function startTimer () {
-    setTimer();
-// display questions
+function makeQuestions (questions) {
+    var qList = quizDiv.createElement("ol"),
+        qArray = JSON.parse(questions);
+        
+    for (var q = 0; q < qArray.length; q++)  {
+        var question = quizDiv.createElement("li");
+
+        question.appendChild(document.createTextNode(questions[q]));
+        qList.appendChild(question);
+    }
+    return qList;
 }
 
 
